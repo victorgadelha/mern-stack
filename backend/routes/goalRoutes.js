77aa69b0199f52c9abe0396194c6b1a26e3ee3c8
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-02-24 10:52:40 -0300

    feat: criando arquivo de controllers para as goals

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -5,4 +5,16 @@ router.get('/', (req, res) => {
   res.status(200).json({ message: 'get goals' });
 });
 
+router.post('/', (req, res) => {
+  res.status(200).json({ message: 'set goals' });
+});
+
+router.put('/:id', (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+});
+
+router.delete('/', (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+});
+
 module.exports = router;
