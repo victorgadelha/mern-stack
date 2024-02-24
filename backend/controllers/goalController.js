467cd578a14dd325c174a6baa41fe0ad6044452e
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-02-24 11:55:24 -0300

    feat: criando middleware de error

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -2,6 +2,7 @@
 // @route GET /api/goals
 // @access Private
 const getGoals = (req, res) => {
+  console.log(req.body);
   res.status(200).json({ message: 'Get goals' });
 };
 
@@ -10,6 +11,10 @@ const getGoals = (req, res) => {
 // @access Private
 
 const setGoal = (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error('Please add a text field');
+  }
   res.status(200).json({ message: 'Set goals' });
 };
 
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 const goalRoutes = require('./routes/goalRoutes');
 
 app.use('/api/goals', goalRoutes);
